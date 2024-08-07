283c5ac2cc455a4223acaf5e84da5840294bb0b8
Ramil <ramil345655@gmail.com>
2024-08-07 17:49:27 +0300

    Wed Aug  7 17:49:27 MSK 2024

--- scr/setup-fonts.sh
+++ scr/setup-fonts.sh
@@ -1,4 +1,4 @@
 # Installing fonts	
 echo "SETUP - Installing fonts..."
-sudo pacman -Sy otf-font-awesome noto-fonts noto-fonts-emoji
+sudo pacman -Sy otf-font-awesome noto-fonts noto-fonts-emoji ttf-fantasque-sans-mono
 echo
